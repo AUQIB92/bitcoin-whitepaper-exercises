26911cf0cbb1ec59c8af50fb49b705d13e88568b
auqib92 <auqib92@gmail.com>
2021-09-18 10:04:22 +0530

    Updated verification Logic

--- hashing/hashing.js
+++ hashing/hashing.js
@@ -18,53 +18,60 @@ var poem = [
 var Blockchain = {
 	blocks: [],
 };
+const genBlk = {
+	index: 0,
+	prevHash: "000000",
+	data: "",
+	timestamp: Date.now()
+
+}
+genBlk.hash = blockHash(genBlk)
 
 // Genesis block
-Blockchain.blocks.push({
-	index: 0,
-	hash: "000000",
-	data: "",
-	timestamp: Date.now(),
-});
+Blockchain.blocks.push(genBlk);
 
 // TODO: insert each line into blockchain
- for (let line of poem) {
-	
-	 Blockchain.blocks.push(createBlock(line))
-		 
-	 
- }
- function createBlock(data)
- {
-	let blk= {
-		index:Blockchain.blocks.length,
-		prevHash: Blockchain.blocks[Blockchain.blocks.length-1].hash,
-		data:data,
+for (let line of poem) {
+
+	Blockchain.blocks.push(createBlock(line))
+
+
+}
+function createBlock(data) {
+	let blk = {
+		index: Blockchain.blocks.length,
+		prevHash: Blockchain.blocks[Blockchain.blocks.length - 1].hash,
+		data: data,
 		timestamp: Date.now
 
-	 }
-	 blk.hash=blockHash(blk);
-	 console.log("Hash of Block",Blockchain.blocks.length-1,"is",blk.hash)
-	 console.log("Prev-Hash of Block",Blockchain.blocks.length-1,"is",blk.prevHash)
-	 return blk;
- }
-
- console.log(`Blockchain is valid: ${verifyChain(Blockchain)}`);
-function verifyChain(blockchain)
-{
-	let valid=false
-	if(blockchain.blocks[0].prevHash === "000000")
-	for(let i=1;i<=blockchain.blocks.length;i++){
-			if(blockchain.blocks[i].prevHash===blockchain.blocks[i-1].hash)
-				valid=true
+	}
+	blk.hash = blockHash(blk);
+	console.log("Hash of Block", Blockchain.blocks.length, "is", blk.hash)
+	console.log("Prev-Hash of Block", Blockchain.blocks.length, "is", blk.prevHash)
+	return blk;
 }
-return valid;
+//console.log(Blockchain)
+console.log(verifyChain(Blockchain));
+function verifyChain(blockchain) {
+	
+
+	if (blockchain.blocks[0].prevHash === "000000") {
+
+		for (let i = 1; i <= blockchain.blocks.length; i++) {
+			if(blockchain.blocks[i].prevHash === blockchain.blocks[i - 1].hash)
+			return true
+			else
+			return false
+		}
+	}
+
+	return false;
 }
 
 // **********************************
 
 function blockHash(bl) {
 	return crypto.createHash("sha256").update(
-	`${bl.index},;${bl.prevHash};${JSON.stringify(bl.data)};${bl.timestamp}`
+		`${bl.index},;${bl.prevHash};${JSON.stringify(bl.data)};${bl.timestamp}`
 	).digest("hex");
 }
